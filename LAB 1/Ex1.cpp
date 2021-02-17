eb0214c06102f9740ddc34b30fc39eb340792e3a
Teodor Socea <teodor.soceas@gmail.com>
2021-02-17 09:21:55 +0200

    Fixed conversion

--- LAB 1/Ex1.cpp
+++ LAB 1/Ex1.cpp
@@ -3,20 +3,21 @@
 using namespace std;
 int char_to_int(char buffer[]){
     int output = 0;
-    for(int i = 0; i < strlen(buffer); i++){
-        output = (buffer[i]-'0') + output*10;
+    for(int i = 0; i < strlen(buffer)-1; i++){
+        output = output * 10 + buffer[i] - '0';
     }
+    printf("%d\n", output);
     return output;
 }
 
 int main(){
     FILE *fptr = fopen("in.txt", "r");
     if(fptr == NULL){
-        printf("File open failed.");
+        printf("File open failed.\n");
         return 0;
     }
     else{
-        printf("File open successful.");
+        printf("File open successful.\n");
         char buffer[100];
         int sum = 0;
         while(fgets(buffer, 100, fptr)!=NULL){
