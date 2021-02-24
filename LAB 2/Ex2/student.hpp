53c7bc3b3ce4c1ec7c9130362e2e086128b17554
Teodor Socea <teodor.soceas@gmail.com>
2021-02-24 08:31:44 +0200

    Some tidying, started work on ex2

---
+++ LAB 2/Ex2/student.hpp
@@ -0,0 +1,19 @@
+struct Catalog{
+    float math, eng, his;
+};
+class Student{
+    char* name;
+    Catalog grades;
+    float avg;
+    public:
+        void setName(char*);
+        void setMath(float);
+        void setEng(float);
+        void setHis(float);
+
+        char* getName();
+        float getMath();
+        float getEng();
+        float getHis();
+        float getAbg();
+};
