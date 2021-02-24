11c79a3e0f1f1247ce39fa8089e7fd5ac8f2c943
Teodor Socea <teodor.soceas@gmail.com>
2021-02-24 08:15:25 +0200

    Update NumberList.cpp

--- LAB 2/NumberList.cpp
+++ LAB 2/NumberList.cpp
@@ -5,3 +5,13 @@
 void NumberList::Init(){
     this->count = 0;
 }
+
+bool NumberList::Add(int x){
+    if(this->count != 10){
+        this->numbers[this->count] = x;
+        this->count++;
+        return true;
+    }
+    else
+        return false;
+}
