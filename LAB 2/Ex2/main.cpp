5c2549205de7e80dea05f73638dd2260ad5d896e
Teodor Socea <teodor.soceas@gmail.com>
2021-02-24 08:58:58 +0200

    Something's really wrong with how I read my students

--- LAB 2/Ex2/main.cpp
+++ LAB 2/Ex2/main.cpp
@@ -9,13 +9,13 @@ int main(){
     scanf("%s", name);
     s1.setName(name);
     printf("Math grade: ");
-    scanf("%f", grade);
+    scanf("%f", &grade);
     s1.setMath(grade);
     printf("English grade: ");
-    scanf("%f", grade);
+    scanf("%f", &grade);
     s1.setEng(grade);
     printf("History grade: ");
-    scanf("%f", grade);
+    scanf("%f", &grade);
     s1.setHis(grade);
 
     s1.printGrades();
