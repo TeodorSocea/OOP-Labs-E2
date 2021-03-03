30c82f99d0f8e23a09b24bba147538e534d3287f
Teodor Socea <teodor.soceas@gmail.com>
2021-03-03 08:10:23 +0200

    Created templated files

---
+++ LAB 3/Ex 1/Math.h
@@ -0,0 +1,27 @@
+class Math
+
+{
+
+public:
+
+    static int Add(int,int);
+
+    static int Add(int,int,int);
+
+    static int Add(double,double);
+
+    static int Add(double,double,double);
+
+    static int Mul(int,int);
+
+    static int Mul(int,int,int);
+
+    static int Mul(double,double);
+
+    static int Mul(double,double,double);
+
+    static int Add(int count,...); // sums up a list of integers
+
+    static char* Add(const char *, const char *);
+
+};
