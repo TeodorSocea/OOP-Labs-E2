f1d727a88fe9509cdb0401de587b97b81446d7fb
Teodor Socea <teodor.soceas@gmail.com>
2021-03-03 08:20:23 +0200

    Added variadic function that works!

--- LAB 3/Ex 1/Math.cpp
+++ LAB 3/Ex 1/Math.cpp
@@ -1,5 +1,5 @@
 #include "Math.h"
-
+#include <cstdarg>
 int Math::Add(int a, int b){
     return a + b;
 }
@@ -22,4 +22,23 @@ int Math::Mul(int a, int b){
 
 int Math::Mul(int a, int b, int c){
     return a * b * c;
+}
+
+int Math::Mul(double a, double b){
+    return a * b;
+}
+
+int Math::Mul(double a, double b, double c){
+    return a * b * c;
+}
+
+int Math::Add(int count, ...){
+    int r = count;
+    va_list va;
+    va_start(va, count);
+    while(int v = va_arg(va, int)){
+        r += v;
+    }
+    va_end(va);
+    return r;
 }
---
+++ LAB 3/Ex 1/main.cpp
@@ -0,0 +1,6 @@
+#include "Math.h"
+#include <iostream>
+int main(){
+    Math mathObj;
+    std::cout<<mathObj.Add(1, 2 ,3 ,4);
+}
