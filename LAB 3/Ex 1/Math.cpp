eddd6af2785f896c55694f7c909191f35285f106
Teodor Socea <teodor.soceas@gmail.com>
2021-03-03 08:26:15 +0200

    Added const char function that also works!

--- LAB 3/Ex 1/Math.cpp
+++ LAB 3/Ex 1/Math.cpp
@@ -42,3 +42,30 @@ int Math::Add(int count, ...){
     va_end(va);
     return r;
 }
+
+char* Math::Add(const char* a , const char* b){
+    char output[1000];
+    const char* maxnum = strlen(a) > strlen(b) ? a : b;
+    const char* minnum = strlen(a) > strlen(b) ? b : a;
+    int i = strlen(maxnum);
+    int j = strlen(minnum);
+    int k = 0;
+    int carry = 0;
+    while(j != 0){
+        output[k++] = (maxnum[i-1] - '0' + minnum[j-1] - '0') % 10 + carry + '0' ;
+        carry = (maxnum[i-1] - '0' + minnum[j-1] - '0') /10 != 0 ? 1 : 0;
+        i--;
+        j--;
+    }
+    while(i){
+        output[k++] = maxnum[i-1];
+        i--;
+    }
+    output[k] = '\0';
+    for(int x = 0, y = k-1; x<y; x++, y--){
+        char aux = output[x];
+        output[x] = output[y];
+        output[y] = aux;
+    }
+    return output;
+}
--- LAB 3/Ex 1/main.cpp
+++ LAB 3/Ex 1/main.cpp
@@ -3,4 +3,6 @@
 int main(){
     Math mathObj;
     std::cout<<mathObj.Add(1, 2 ,3 ,4);
+    std::cout<<'\0';
+    std::cout<<mathObj.Add("1234", "1234");
 }
