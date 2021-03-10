84e2d7045f01efb00182e0813a49f84d885c0990
Teodor Socea <teodor.socea@gmail.com>
2021-03-10 08:22:07 +0200

    added intializer list constructor

--- LAB 4/Ex1/Sort.cpp
+++ LAB 4/Ex1/Sort.cpp
@@ -1,6 +1,7 @@
 #include "Sort.h"
 #include <stdio.h>
 #include <cstdlib>
+#include <iterator>
 
 void Sort::Print(){
     for(int i = 0 ; i < this->count; i++)
@@ -22,4 +23,9 @@ Sort::Sort(int count, int mini, int maxi){
     for(int i = 0 ; i < this->count; i++){
         this->data[i] = rand() % (maxi - mini + 1) + mini; 
     }
+}
+
+Sort::Sort(std::initializer_list<int> list) : data(new int[list.size()]){
+    std::copy(std::begin(list), std::end(list), this->data);
+    this->count = list.size();
 }
--- LAB 4/Ex1/main.cpp
+++ LAB 4/Ex1/main.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 
 int main(){
-    Sort elements(10, 0, 10);
+    Sort elements = {1, 2, 3, 4, 5, 6, 7, 8};
     elements.Print();
 }
