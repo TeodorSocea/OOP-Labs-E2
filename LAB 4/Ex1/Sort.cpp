a8e558efd3f961cc0637e0f707e838d1dae20e3b
Teodor Socea <teodor.socea@gmail.com>
2021-03-10 08:30:24 +0200

    Added variadic parameter constructor

--- LAB 4/Ex1/Sort.cpp
+++ LAB 4/Ex1/Sort.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <cstdlib>
 #include <iterator>
+#include <cstdarg>
 
 void Sort::Print(){
     for(int i = 0 ; i < this->count; i++)
@@ -36,3 +37,13 @@ Sort::Sort(int count, int* input_array){
     for(int i = 0; i < this->count; i++)
         this->data[i] = input_array[i];
 }
+
+Sort::Sort(int count...){
+    this->count = count;
+    this->data = new int[this->count];
+    va_list args;
+    va_start(args, count);
+    for(int i = 0 ; i < this->count; i++)
+        this->data[i] = va_arg(args, int);
+    va_end(args);
+}   
--- LAB 4/Ex1/main.cpp
+++ LAB 4/Ex1/main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main(){
     int p[] = {1, 2, 3, 4, 5, 6, 7};
-    Sort elements(7, p);
+    Sort elements(5, 1, 2, 3, 4, 5);
     elements.Print();
 }
