6d483f31deef95beadc219159ac7cd7afbc1b4d5
Teodor Socea <teodor.socea@gmail.com>
2021-03-10 08:11:01 +0200

    added elements count method

--- LAB 4/Ex1/Sort.cpp
+++ LAB 4/Ex1/Sort.cpp
@@ -1,7 +1,14 @@
 #include "Sort.h"
 #include <stdio.h>
+
 void Sort::Print(){
     for(int i = 0 ; i < this->count; i++)
         printf("%d ", this->data[i]);
     printf("\n");
 }
+
+int Sort::GetElementsCount(){
+    return this->count;
+}
+
+
