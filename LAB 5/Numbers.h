70dfb37e7eb2155c96e537ad56085c837ab190da
Teodor Socea <teodor.socea@gmail.com>
2021-03-17 08:06:25 +0200

    Defined the class, copy constructor and move constructor

---
+++ LAB 5/Numbers.h
@@ -0,0 +1,24 @@
+class Number
+
+{
+
+    char *value;
+    int base;
+
+public:
+    Number(const char *value, int base); // where base is between 2 and 16
+
+    ~Number();
+
+    Number(const Number &n);
+
+    Number(Number &&n);
+
+    void SwitchBase(int newBase);
+
+    void Print();
+
+    int GetDigitsCount(); // returns the number of digits for the current number
+
+    int GetBase(); // returns the current base
+}
