434d5791d24cfd4797cea0076053d7325299ff24
Teodor Socea <teodor.socea@gmail.com>
2021-03-17 08:45:17 +0200

    Added switchBase method

--- LAB 5/Number.cpp
+++ LAB 5/Number.cpp
@@ -1,10 +1,11 @@
 #include "Numbers.h"
 #include <cstring>
 #include <stdio.h>
+#include <ctype.h>
 Number::Number(const char *value, int base)
 {
-    this->value = new char[strlen(value)];
-    memcpy(this->value, value, strlen(value) * sizeof(char));
+    this->value = new char[strlen(value) + 1];
+    memcpy(this->value, value, (strlen(value) + 1) * sizeof(char));
     this->base = base;
 }
 
@@ -47,4 +48,28 @@ int Number::GetDigitsCount()
 int Number::GetBase()
 {
     return this->base;
+}
+
+void Number::SwitchBase(int newBase)
+{
+    printf("Debugging\n");
+    char output[1000];
+    int X = this->base;
+    int value = 0, i = 0;
+    for (int k = 0; k < strlen(this->value); k++)
+    {
+        printf("Debugging for\n");
+        value = (X * value) + (isalpha(this->value[i]) ? (this->value[i] - 'A' + 10) : (this->value[i] - '0'));
+    }
+    while (value)
+    {
+        printf("Debugging while\n");
+        output[i] = (value % newBase < 10) ? ((value % newBase) + '0') : ((value % newBase) - 10 + 'A');
+        value /= newBase;
+        i++;
+    }
+    printf("Debugging out\n");
+    memcpy(this->value, output, strlen(output) * sizeof(output));
+    this->base = newBase;
+    printf("Debugging end\n");
 }
---
+++ LAB 5/main.cpp
@@ -0,0 +1,10 @@
+#include "Numbers.h"
+
+int main()
+{
+
+    Number a("111", 2);
+    a.Print();
+    a.SwitchBase(10);
+    a.Print();
+}
