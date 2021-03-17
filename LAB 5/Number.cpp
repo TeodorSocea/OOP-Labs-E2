9cf932760231ecbfb85fee104c36ffa2d9678a2c
Teodor Socea <teodor.socea@gmail.com>
2021-03-17 08:10:38 +0200

    added print method

--- LAB 5/Number.cpp
+++ LAB 5/Number.cpp
@@ -1,8 +1,14 @@
 #include "Numbers.h"
 #include <cstring>
+#include <stdio.h>
 Number::Number(const char *value, int base)
 {
     this->value = new char[strlen(value)];
     memcpy(this->value, value, strlen(value) * sizeof(char));
     this->base = base;
+}
+
+void Number::Print()
+{
+    printf("Base: %d, Value: %s\n", this->base, this->value);
 }
