9c6cf2f665c8ef34b8ace0ee9482a25111a55ac4
Teodor Socea <teodor.socea@gmail.com>
2021-03-24 08:40:37 +0200

    Added definitions for dacia

---
+++ LAB 6/Dacia.cpp
@@ -0,0 +1,42 @@
+#include "Dacia.h"
+#include "Weather.h"
+void Dacia::SetFuelCapacity(int cap)
+{
+    this->fuelCapacity = cap;
+}
+void Dacia::SetFuelConsumption(int con)
+{
+    this->fuelConsumption = con;
+}
+void Dacia::SetAvgSpeed(int spd)
+{
+    this->avgSpeed = spd;
+}
+Dacia::Dacia()
+{
+    this->SetFuelCapacity(100);
+    this->SetFuelConsumption(2);
+    this->SetAvgSpeed(2);
+}
+int Dacia::CalculateScore(int length, Weather w)
+{
+    int modifier;
+    switch (w)
+    {
+    case Rain:
+        modifier = 0;
+        break;
+    case Sunny:
+        modifier = 1;
+        break;
+    case Snow:
+        modifier = -1;
+        break;
+    }
+    int reqSteps = length / (this->avgSpeed + modifier);
+    int posSteps = this->fuelCapacity / this->fuelConsumption;
+    if (posSteps >= reqSteps)
+        return reqSteps;
+    else
+        return -1;
+}
