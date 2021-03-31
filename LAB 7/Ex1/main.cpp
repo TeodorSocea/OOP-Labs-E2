117aa1a1853dd7e259b89797ad0162d5b9272866
Teodor Socea <teodor.socea@gmail.com>
2021-03-31 10:01:42 +0300

    Update main.cpp

--- LAB 7/Ex1/main.cpp
+++ LAB 7/Ex1/main.cpp
@@ -2,11 +2,11 @@
 #include <stdio.h>
 float operator""_Kelvin(const char *val)
 {
-    return val[0] == '-' ? -atof(val) : atof(val) + 273.15;
+    return atof(val) + 273.15;
 }
 float operator""_Fahrenheit(const char *val)
 {
-    return (val[0] == '-' ? -atof(val) : atof(val) * 1.8) + 32;
+    return (atof(val) * 1.8) + 32;
 }
 
 int main()
