d806a00748082abb3516189eb1e2f5db39c3aec3
Teodor Socea <teodor.socea@gmail.com>
2021-03-31 08:10:38 +0300

    Fahrenheit literal added

--- LAB 7/Ex1/main.cpp
+++ LAB 7/Ex1/main.cpp
@@ -4,3 +4,7 @@ float operator""_Kelvin(const char *val)
 {
     return val[0] == '-' ? -atof(val) : atof(val) + 273.15;
 }
+float operator""_Fahrenheit(const char *val)
+{
+    return (val[0] == '-' ? -atof(val) : atof(val) * 1.8) + 32;
+}
