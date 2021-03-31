4b4a003303ecfd4bb637bfc310f1b3e90e416701
Teodor Socea <teodor.socea@gmail.com>
2021-03-31 08:13:47 +0300

    Exercise done

--- LAB 7/Ex1/main.cpp
+++ LAB 7/Ex1/main.cpp
@@ -8,3 +8,15 @@ float operator""_Fahrenheit(const char *val)
 {
     return (val[0] == '-' ? -atof(val) : atof(val) * 1.8) + 32;
 }
+
+int main()
+{
+    float a = 300_Kelvin;
+    float b = 120_Fahrenheit;
+    float c = -100_Kelvin; //unfortunately this doesn't work because literals can't take negative values
+    float d = -100_Fahrenheit;
+
+    printf("%f %f %f %f\n", a, b, c, d);
+
+    return 0;
+}
