b3b594a27d119a3f2c6eb3f2171eaa7d509692fe
Teodor Socea <teodor.socea@gmail.com>
2021-04-14 08:20:05 +0300

    Added splitting of the string into a string vector

--- LAB 8/main.cpp
+++ LAB 8/main.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <iterator>
+#include <regex>
 
 using std::ifstream;
+using std::regex;
+using std::remove_if;
+using std::sregex_token_iterator;
 using std::string;
+using std::vector;
 
 ifstream in("in.txt");
+
+vector<string> parsePhrase(const string &str, const regex &re)
+{
+    sregex_token_iterator it(str.begin(), str.end(), re, -1);
+    vector<string> parsed(it, {});
+
+    parsed.erase(remove_if(parsed.begin(), parsed.end(), [](string const &s) { return s.size() == 0; }), parsed.end());
+
+    return parsed;
+}
 
 int main()
 {
     string phrase;
     getline(in, phrase);
-    std::cout << phrase;
+
+    regex re(R"([\s|,|.|?|!]+)");
+
+    vector<string> words = parsePhrase(phrase, re);
+
+    for (auto word : words)
+    {
+        std::cout << word << '\n';
+    }
 }
