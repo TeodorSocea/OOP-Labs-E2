f0f8e820cd01738628478274a566793dee147bdd
Teodor Socea <teodor.socea@gmail.com>
2021-04-21 08:13:02 +0300

    added data members of map

--- LAB 9/map.h
+++ LAB 9/map.h
@@ -12,5 +12,16 @@ private:
         MapEntry(K key, V value) : first(key), second(value) {}
     };
 
+    // map class members
+    MapEntry *MapEntries;
+    int count;
+    int size;
+
 public:
+    Map()
+    {
+        count = 0;
+        size = 1;
+        MapEntries = new MapEntry[size];
+    }
 };
