352ad84408f8b7987f44be9f897e8c5d2c52dccc
Teodor Socea <teodor.socea@gmail.com>
2021-04-21 09:17:58 +0300

    problem done

--- LAB 9/map.h
+++ LAB 9/map.h
@@ -104,6 +104,60 @@ public:
         }
         return false;
     }
+    void set(const K &key, const V &value)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            if (MapEntries[i].first == key)
+            {
+                MapEntries[i].second = value;
+                return;
+            }
+        }
+        if (size == count)
+        {
+            size *= 2;
+            MapEntry *newMapEntries = new MapEntry[size];
+            memcpy(newMapEntries, MapEntries, count * sizeof(MapEntry));
+            delete[] MapEntries;
+            MapEntries = newMapEntries;
+        }
+        count++;
+        MapEntries[count - 1].first = key;
+        MapEntries[count - 1].second = value;
+    }
+    void Clear()
+    {
+        size = 1;
+        count = 0;
+        delete[] MapEntries;
+        MapEntries = new MapEntry[size];
+    }
+    bool Delete(const K &key)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            if (MapEntries[i].first == key)
+            {
+                memcpy(MapEntries + i, MapEntries + i + 1, (--count - 1) * sizeof(MapEntry));
+                return true;
+            }
+        }
+        return false;
+    }
+    bool Include(const Map<K, V> &to_compare)
+    {
+        int matches = 0;
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = 0; j < to_compare.count; j++)
+            {
+                if (MapEntries[i] == to_compare.keys[j])
+                    matches++;
+            }
+        }
+        return matches == to_compare.count ? true : false;
+    }
     MapIterator begin()
     {
         return MapIterator(MapEntries, 0);
